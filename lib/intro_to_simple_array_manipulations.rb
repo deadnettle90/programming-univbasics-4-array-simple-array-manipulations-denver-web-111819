92b2d75beeeb020f3e5c989ebcbf36ee5124d72d
Danielle Matton <danimatton@gmail.com>
2019-10-25 00:17:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,15 +6,15 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 
-def using_pop()
-  
+def using_pop(array)
+  array.pop
 end
 
-def pop_with_args()
-  
+def pop_with_args(array)
+  array.pop(2)
 end
 
-def using_shift()
+def using_shift(array)
   
 end
 
